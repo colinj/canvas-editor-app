7717775af624f57e8cc1de40b7a58199e032dd20
Colin Johnsun <colin.johnsun@gmail.com>
2020-11-17 15:00:10 +1100

    feat: Create application page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div>Brightness & Contrast Developer Test</div>
+    <div>01 Jun, 2018 – 31 Dec, 2019</div>
+  </div>
+  <div class="hero">
+    <img src="@/assets/hero.png" alt="Hero" />
+    <div class="avatar">
+      <img src="@/assets/profile.png" alt="profile pic" />
+    </div>
+  </div>
+  <div class="controls">
+    <div class="input-range">
+      <label for="brightness">Brightness</label>
+      <input
+        type="range"
+        name="brightness"
+        id="brightness"
+        min="-100"
+        max="100"
+        step="1"
+      />
+      <div>Slide to adjust image brightness!☀️</div>
+    </div>
+    <div class="input-range">
+      <label for="contrast">Contrast</label>
+      <input
+        type="range"
+        name="contrast"
+        id="contrast"
+        min="-100"
+        max="100"
+        step="1"
+      />
+      <div>Slide to adjust image contrast! 🌓</div>
+    </div>
+  </div>
+  <div class="main">
+    <div class="image-container">
+      <img :src="imageSource" alt="target image" />
+      <div class="control-footer">
+        <div class="file-loader">
+          <label for="file-loader">Name</label>
+          <input
+            type="file"
+            name="file-loader"
+            id="file-loader"
+            @change="selectFile"
+          />
+        </div>
+        <button @click="uploadFile">Upload</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      selectedFile: null,
+      imageSource: null
+    };
+  },
+  methods: {
+    selectFile(evt) {
+      this.selectedFile = evt.target.files[0];
+    },
+    uploadFile() {
+      this.imageSource = URL.createObjectURL(this.selectedFile);
+    }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+img {
+  width: 100%;
+}
+
+.hero {
+  position: relative;
+}
+
+$avatar-size: 8rem;
+
+.avatar {
+  position: absolute;
+  bottom: -2rem;
+  left: calc(50% - #{$avatar-size / 2});
+  height: $avatar-size;
+  width: $avatar-size;
+  background-color: lightgreen;
+  border: 4px solid #fff;
+  border-radius: 50%;
+  overflow: hidden;
+}
+</style>

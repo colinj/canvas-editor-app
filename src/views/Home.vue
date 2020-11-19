fe8dd1f5d45147724397764569260c55963c3d2b
Colin Johnsun <colin.johnsun@gmail.com>
2020-11-19 14:26:41 +1100

    feat: Add canvas manipulation functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -25,7 +25,13 @@
   </div>
   <div class="main">
     <div class="image-container">
-      <img :src="imageSource" alt="target image" />
+      <img
+        alt="target image"
+        ref="imgSource"
+        style="opacity:0;position:absolute;z-index:-100"
+        @load="copyToCanvas()"
+      />
+      <canvas class="canvas" ref="canvas"></canvas>
       <div class="control-footer">
         <div class="file-loader">
           <label for="file-loader">Name</label>
@@ -47,6 +53,8 @@ import AppHeader from "@/components/AppHeader";
 import HeroSection from "@/components/HeroSection";
 import RangeSlider from "@/components/RangeSlider";
 
+const pipe = (...fns) => initVal => fns.reduce((val, fn) => fn(val), initVal);
+
 export default {
   components: {
     AppHeader,
@@ -58,17 +66,69 @@ export default {
     return {
       brightness: 0,
       contrast: 0,
-      selectedFile: null,
-      imageSource: null
+      context: null,
+      imageData: {}
     };
   },
   methods: {
     selectFile(evt) {
-      this.selectedFile = evt.target.files[0];
+      this.$refs.imgSource.src = URL.createObjectURL(evt.target.files[0]);
     },
-    uploadFile() {
-      this.imageSource = URL.createObjectURL(this.selectedFile);
+    copyToCanvas() {
+      const img = this.$refs.imgSource;
+      URL.revokeObjectURL(img.src);
+      this.$refs.canvas.width = img.width;
+      this.$refs.canvas.height = img.height;
+      this.context.drawImage(img, 0, 0, img.width, img.height);
+      this.imageData = this.context.getImageData(0, 0, img.width, img.height);
+    },
+    createImageData() {
+      const img = this.$refs.imgSource;
+      const imgData = this.context.createImageData(img.width, img.height);
+      imgData.data.set(this.imageData.data);
+      return imgData;
+    },
+    applyBrightness(imgData, brightness) {
+      const data = imgData.data;
+      const factor = brightness * 1.28; // 2.55;
+      for (let i = 0; i < data.length; i += 4) {
+        data[i] += factor;
+        data[i + 1] += factor;
+        data[i + 2] += factor;
+      }
+      return imgData;
+    },
+    applyContrast(imgData, contrast) {
+      const data = imgData.data;
+      const factor =
+        (259.0 * (contrast + 255.0)) / (255.0 * (259.0 - contrast));
+
+      for (var i = 0; i < data.length; i += 4) {
+        data[i] = factor * (data[i] - 128.0) + 128.0;
+        data[i + 1] = factor * (data[i + 1] - 128.0) + 128.0;
+        data[i + 2] = factor * (data[i + 2] - 128.0) + 128.0;
+      }
+      return imgData;
+    },
+    modifyImg() {
+      const imgData = pipe(
+        x => this.applyBrightness(x, this.brightness),
+        x => this.applyContrast(x, this.contrast)
+      )(this.createImageData());
+
+      this.context.putImageData(imgData, 0, 0);
     }
+  },
+  watch: {
+    brightness() {
+      this.modifyImg();
+    },
+    contrast() {
+      this.modifyImg();
+    }
+  },
+  mounted() {
+    this.context = this.$refs.canvas.getContext("2d");
   }
 };
 </script>
